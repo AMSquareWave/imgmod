ce093ab2c5047864dd3143f12dfaab7a8ace7e1d
AMSquareWave <amsquarewave@gmail.com>
2023-10-05 01:33:04 -0500

    Fixed RGBText Function

--- Text/text.go
+++ Text/text.go
@@ -5,5 +5,5 @@ import "fmt"
 func RGBText(redText, greenText, blueText string) {
 	fmt.Println("\033[31m" + redText + "\033[0m")
 	fmt.Println("\033[32m" + greenText + "\033[0m")
-	fmt.Println("\033[33m" + blueText + "\033[0m")
+	fmt.Println("\033[34m" + blueText + "\033[0m")
 }
