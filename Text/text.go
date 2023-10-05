4016c03aa107c0340370e4fafd942f0c3056aeb7
AMSquareWave <amsquarewave@gmail.com>
2023-10-05 09:36:11 -0500

    Added ColourText Function

--- Text/text.go
+++ Text/text.go
@@ -1,9 +1,16 @@
 package Text
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func RGBText(redText, greenText, blueText string) {
 	fmt.Println("\033[31m" + redText + "\033[0m")
 	fmt.Println("\033[32m" + greenText + "\033[0m")
 	fmt.Println("\033[34m" + blueText + "\033[0m")
+}
+
+func ColourText(colourCode int, text string) {
+	fmt.Print("\033[" + strconv.Itoa(colourCode) + "m" + text + "\033[0m")
 }
