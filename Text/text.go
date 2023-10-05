ecb796278f87a07aa754167529d81e7de6e0c8fb
AMSquareWave <amsquarewave@gmail.com>
2023-10-05 00:47:16 -0500

    Added Text Module

---
+++ Text/text.go
@@ -0,0 +1,9 @@
+package text
+
+import "fmt"
+
+func RGBText(redText, greenText, blueText string) {
+	fmt.Println("\033[31m" + redText + "\033[0m")
+	fmt.Println("\033[32m" + greenText + "\033[0m")
+	fmt.Println("\033[33m" + blueText + "\033[0m")
+}
